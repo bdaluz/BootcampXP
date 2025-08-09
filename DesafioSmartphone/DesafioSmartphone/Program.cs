25bd5f032e0e4295415d0170703d10e3f08eea58
bdaluz <143043482+bdaluz@users.noreply.github.com>
2025-08-09 17:27:02 -0300

    Desafio completo.

---
+++ DesafioSmartphone/DesafioSmartphone/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioSmartphone.Models;
+
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "174715978915", modelo: "Nokia G60", imei: "1085884655262947" ,memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Pou");
+nokia.ReceberLigacao();
+
+
+Console.WriteLine("\nIphone:");
+Smartphone iphone = new Iphone(numero: "17455699789", modelo: "Iphone 12", imei: "484844154499666", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Sky Guide");
+iphone.ReceberLigacao();
