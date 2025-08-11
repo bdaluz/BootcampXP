1244244d940dd9e7000183684cbb4f820a10c656
bdaluz <143043482+bdaluz@users.noreply.github.com>
2025-08-11 14:08:59 -0300

    DesafioAPI completo

---
+++ DesafioAPI/DesafioAPI/DTOs/AtualizarTarefaDTO.cs
@@ -0,0 +1,22 @@
+﻿using DesafioAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesafioAPI.DTOs
+{
+    public class AtualizarTarefaDTO
+    {
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [MaxLength(200)]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [MaxLength(2000)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public EnumStatusTarefa Status { get; set; }
+    }
+}
